9c70fa054837fd086cea7a04222ba874bb4c5575
caveman49 <caveman4949@gmail.com>
2021-10-14 15:42:01 -0400

    added_functions_and_loops_to_display_selected_movie_information

--- script.js
+++ script.js
@@ -7,8 +7,24 @@ fetch(`https://ghibliapi.herokuapp.com/films`)
 
     for (let movie of movies) {
       const option = document.createElement("option");
-      option.setAttribute("value", movie.original_title_romanised);
-      option.textContent = movie.original_title_romanised;
+      option.setAttribute("value", movie.title);
+      option.textContent = movie.title;
       dropDown.append(option);
     }
+
+    const currentMovie = document.querySelector("#title");
+    const movieYear = document.querySelector("#year");
+    const movieDescription = document.querySelector("#description");
+    let selectedMovie;
+
+    dropDown.addEventListener("change", () => {
+      for (let movie of movies) {
+        if (dropDown.value === movie.title) {
+          selectedMovie = movie;
+          currentMovie.textContent = movie.title
+          movieYear.textContent = movie.release_date
+          movieDescription.textContent = movie.description
+        }
+      }
+    });
   });
